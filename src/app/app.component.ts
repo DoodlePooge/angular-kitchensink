e03d9dc5f79a832d8b29f90468389e3e9f3480a1
DoodlePooge <paige.maute@gmail.com>
2024-02-22 01:06:20 -0500

    attempt at styling/theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { NavBarComponent } from "./nav-bar/nav-bar.component"
+import { ButtonsComponent } from './buttons/buttons.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, ButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
